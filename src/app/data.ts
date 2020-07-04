7e2055a1da52568685fee0906cdb6f7fc6d2913f
NilsonCapetillo <62961588+NilsonCapetillo@users.noreply.github.com>
2020-07-04 12:37:58 -0500

    Create data.ts

---
+++ src/app/data.ts
@@ -0,0 +1,68 @@
+import { Automovil } from './models';
+import {Country} from './models';
+
+export const AUTOMOVILES: Automovil[] = [
+    // {
+    //     marca: 'Toyota',
+    //     submarca: 'Corolla',
+    //     modelo: 2010,
+    //     descripcion: 'Toyota Corolla 4 Puertas Estandar',
+    //     fecha_registro: new Date('2020-03-10')
+    // },
+    // {
+    //     marca: 'Honda',
+    //     submarca: 'Civic',
+    //     modelo: 2018,
+    //     descripcion: 'Honda Civic 4 Puertas Automático',
+    //     fecha_registro: new Date('2020-03-10')
+    // },
+    // {
+    //     marca: 'Honda',
+    //     submarca: 'Civic',
+    //     modelo: 2018,
+    //     descripcion: 'Honda Civic 4 Puertas Estandar',
+    //     fecha_registro: new Date('2020-03-10')
+    // },
+    // {
+    //     marca: 'Toyota',
+    //     submarca: 'Camry',
+    //     modelo: 2010,
+    //     descripcion: 'Toyota Camry 4 Puertas Estandar',
+    //     fecha_registro: new Date('2020-03-15')
+    // },
+    // {
+    //     marca: 'Honda',
+    //     submarca: 'City',
+    //     modelo: 2010,
+    //     descripcion: 'Honda City 5 Puertas Automático Asientos Piel',
+    //     fecha_registro: new Date('2020-03-15')
+    // }
+
+]
+
+export const COUNTRIES: Country[] = [
+    {
+      name: 'Russia',
+      flag: 'f/f3/Flag_of_Russia.svg',
+      area: 17075200,
+      population: 146989754
+    },
+    {
+      name: 'Canada',
+      flag: 'c/cf/Flag_of_Canada.svg',
+      area: 9976140,
+      population: 36624199
+    },
+    {
+      name: 'United States',
+      flag: 'a/a4/Flag_of_the_United_States.svg',
+      area: 9629091,
+      population: 324459463
+    },
+    {
+      name: 'China',
+      flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
+      area: 9596960,
+      population: 1409517397
+    }
+  ]
