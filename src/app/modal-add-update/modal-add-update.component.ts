3dce581d460b47d75dad3beea7d2f746550629e5
NilsonCapetillo <62961588+NilsonCapetillo@users.noreply.github.com>
2020-07-04 15:37:46 -0500

    Create modal-add-update.component.ts

---
+++ src/app/modal-add-update/modal-add-update.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Automovil } from '../models';
+@Component({
+  selector: 'app-modal-add-update',
+  templateUrl: './modal-add-update.component.html',
+  styleUrls: ['./modal-add-update.component.css']
+})
+export class ModalAddUpdateComponent implements OnInit {
+
+  //Agregar o Editar
+  accion: string;
+  auto: Automovil;
+  constructor(public activeModal: NgbActiveModal) { }
+
+  ngOnInit(): void {
+  }
+
+}
