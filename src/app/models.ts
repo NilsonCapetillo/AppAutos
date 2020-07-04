6e528903a73783ea42135349a055becb5660e33c
NilsonCapetillo <62961588+NilsonCapetillo@users.noreply.github.com>
2020-07-04 15:20:39 -0500

    cambie el id a como esta en la api (_id)

---
+++ src/app/models.ts
@@ -0,0 +1,13 @@
+export interface Automovil {
+    _id: number;
+    marca: string;
+    submarca: string;
+    modelos: number[];
+    descripcion: string;
+}
+export interface Country {
+    name: string;
+    flag: string;
+    area: number;
+    population: number;
+  }
