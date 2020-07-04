b128836ab5f4d28976d80fd1cd8db7653321d5c1
NilsonCapetillo <62961588+NilsonCapetillo@users.noreply.github.com>
2020-07-04 15:37:55 -0500

    Create modal-confirm-action.component.ts

---
+++ src/app/modal-confirm-action/modal-confirm-action.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Automovil } from '../models';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-modal-confirm-action',
+  templateUrl: './modal-confirm-action.component.html',
+  styleUrls: ['./modal-confirm-action.component.css']
+})
+export class ModalConfirmActionComponent implements OnInit {
+
+  mensaje: string;
+  auto: Automovil;
+
+  constructor(public activeModal: NgbActiveModal) { }
+
+  ngOnInit(): void {
+  }
+
+}
